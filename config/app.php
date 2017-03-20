ec669ee8f282fc2c4484daaeda3eb368feafbd5a
neverbehave <luoxinhao77@gmail.com>
2017-03-20 18:42:05 +0800

    database init

--- config/app.php
+++ config/app.php
@@ -176,6 +176,11 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        /**
+         * External Provider
+         */
+        browner12\helpers\HelperServiceProvider::class,
+
 
     ],
 
--- config/database.php
+++ config/database.php
@@ -98,10 +98,17 @@ return [
         'client' => 'predis',
 
         'default' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => 0,
+	        'host'     => env('REDIS_HOST', '127.0.0.1'),
+	        'password' => env('REDIS_PASSWORD', NULL),
+	        'port'     => env('REDIS_PORT', 6379),
+	        'database' => 0,
+        ],
+
+        'session' => [
+	        'host'     => env('REDIS_HOST', '127.0.0.1'),
+	        'password' => env('REDIS_PASSWORD', NULL),
+	        'port'     => env('REDIS_PORT', 6379),
+	        'database' => 1,
         ],
 
     ],

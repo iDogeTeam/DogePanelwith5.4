a7e411b701df24c4eefe6b3612addce54e645b65
NeverBehave <luoxinhao77@gmail.com>
2017-03-21 09:53:16 +0800

    middleware example

--- app/Http/Middleware/VerifyAdmin.php
+++ app/Http/Middleware/VerifyAdmin.php
@@ -3,18 +3,25 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Facades\Log;
 
 class VerifyAdmin
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        return $next($request);
-    }
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @param  \Closure $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+		if (Auth::check() && Auth::user()->role === 'admin') {
+			return $next($request);
+		}
+		abort(403, Lang::get('auth.credential_required'));
+		Log::notice('failed admin operation from IP "' . $request->ip() . '"');
+	}
 }

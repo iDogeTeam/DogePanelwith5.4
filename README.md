547c38e58c2cc311d0d14a2038b8b3b5b245d2bc
huihuimoe <0w0@loli.pet>
2017-03-23 12:45:48 +0800

    configure npm, webpack and eslint.

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ __reconstructing based on orvice/ss-panel__
 - combination ansible
 
 
+# Fortend
+
+Based on [vue 2](https://github.com/vuejs/vue) and [element-ui](https://github.com/ElemeFE/element).
+
+You can use these command to dev you own frontend ui.
+
+- dev `npm run dev`
+- build `npm run build`
+- test `npm test`
---
+++ test/unit/specs/App.spec.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Example from '@/components/Example'
+
+describe('Example.vue', () => {
+  it('should render correct contents', () => {
+    const Constructor = Vue.extend(Example)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('.panel-body').textContent)
+      .to.equal('I\'m an example component!')
+  })
+})

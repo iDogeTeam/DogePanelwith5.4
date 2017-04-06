9d99ce1b3328695a96cfef2fdd079885d218d34e
huihuimoe <0w0@loli.pet>
2017-04-06 17:28:07 +0800

    update packages version & use new vue-i18n & update router structures
    🔧Frontend: add navbar

--- resources/assets/app.js
+++ resources/assets/app.js
@@ -4,7 +4,7 @@ import VueRouter from 'vue-router'
 import routes from './routes'
 import Element from 'element-ui'
 import VueI18n from 'vue-i18n'
-import i18n from './i18n'
+import messages from './i18n'
 
 // configure vue-router
 Vue.use(VueRouter)
@@ -18,15 +18,16 @@ Vue.use(Element)
 
 // configure vue-i18n
 Vue.use(VueI18n)
-Vue.config.lang = 'en'
-Vue.config.fallbackLang = 'en'
-for (let lang in i18n) {
-    Vue.locale(lang, i18n[lang])
-}
+const i18n = new VueI18n({
+    locale: 'en',
+    fallbackLocale: 'en',
+    messages
+})
 
 // init vue and mount vue instance to real dom
 new Vue({
     router,
+    i18n,
     el: '#root',
     render: h => h(app)
 })
